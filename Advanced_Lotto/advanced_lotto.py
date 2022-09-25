d5e2ba7d45260bacdef4cea4ee43e2dad73843f6
Grandy005 <90681865+Grandy005@users.noreply.github.com>
2022-09-25 14:39:22 +0200

    Add files via upload

---
+++ Advanced_Lotto/advanced_lotto.py
@@ -0,0 +1,35 @@
+from functools import cache
+from random import randint
+from time import perf_counter
+
+my_numbers = []
+random_numbers = []
+counter = 0
+tries = 0
+
+while True:
+    num = int(input('Adj meg egy számot 1 és 90 között: '))
+    if 0 < num < 91 and num not in my_numbers:
+        my_numbers.append(num)
+        counter += 1
+    else:
+        print('HIBA!')
+    if counter == 5:
+        break
+
+start = perf_counter()
+
+while True:
+    tries += 1
+    for x in range(5):
+        random_numbers.append(randint(1, 90))
+    check =  all(item in random_numbers for item in my_numbers)
+    if check == True:
+        end = perf_counter()
+        break
+    else:
+        random_numbers.clear()
+
+print(random_numbers)
+print('Sorsolások száma:', tries)
+print(f'Sorsolás alatt eltelt idő: {round(end-start, 2)} mp')
